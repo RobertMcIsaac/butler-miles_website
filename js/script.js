c8cebf415c93a5fc3f49dd97aa2dafbdf50f2179
RobMcI <114496674+RobertMcIsaac@users.noreply.github.com>
2024-03-01 17:50:47 +0000

    Vintage card fix. anchor scroll links fix in script.js.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,23 @@
 // MAIN
+
 // calculate navbar height and apply to <main> margin-top 
 document.addEventListener('DOMContentLoaded', function() {
-    const navbarHeight = document.querySelector('.navbar').offsetHeight;
-    document.querySelector('.nav-height-offset').style.marginTop = `${navbarHeight}px`;
+  const navbarHeight = document.querySelector('.navbar').offsetHeight;
+  document.querySelector('.nav-height-offset').style.marginTop = `${navbarHeight}px`;
+
+  // Handle click events on anchor links
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+      anchor.addEventListener('click', function(e) {
+          e.preventDefault();
+
+          const targetId = this.getAttribute('href');
+          const targetElement = document.querySelector(targetId);
+
+          // Adjust scroll position to account for navbar height
+          window.scroll({
+              top: targetElement.offsetTop - navbarHeight, // Adjust scroll position
+              behavior: 'smooth'
+          });
+      });
   });
+});
